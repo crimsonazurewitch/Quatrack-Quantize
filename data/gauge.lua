6604cc7aff51cf7521611227c74de0b38712a705
LuIvory (FMichael) <m.placeholder.m@gmail.com>
2026-09-24 08:17:11 +0200

    Castigate the house ---- Added a bit of Gauge Functionality Most Gauges don't function properly as of now, this includes:     Clear     Drain     Fatalis     Hex ---- I'll work on this more after I'm done with CAW. As for now, I'll hop in and figure things out to take a break from it. - FM

---
+++ data/gauge.lua
@@ -0,0 +1,13 @@
+PieceData = { -- If you want to add more, keep these in mind: game.lua (174, 2485, 2770), tower.lua (1670, 1678), panel.lua
+    { id = 'amelie',	popup = { COLOR.lR, "Hex Gauge" } },
+    { id = 'jade',    popup = { COLOR.lG, "Stress Gauge" } },
+    { id = 'lena', popup = { COLOR.lB, "Clear Gauge" } },
+    { id = 'august',  popup = { COLOR.lO, "Standard Gauge" } },
+    { id = 'mateo',   popup = { COLOR.lM, "No Gauge" } },
+    { id = 'onetap', popup = { COLOR.lY, "O - Invisible Card" } },
+    { id = 'closeCard', popup = { COLOR.lC, "I - Close Card" } },
+    { id = 'noHit',     popup = { COLOR.lA, "P - No-Hit" } },
+    { id = 'ryoshu',    popup = { COLOR.lF, "R - Redaction" } },
+    { id = 'graceless',	popup = { COLOR.lI, "G - Graceless"} },
+
+}
